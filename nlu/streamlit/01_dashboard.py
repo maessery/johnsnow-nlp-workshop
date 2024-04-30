0803f371cda6b43fc505654e4a7d9820e29dea9c
maessery <35372325+maessery@users.noreply.github.com>
2024-04-29 23:13:24 -0400

    Delete nlu directory

--- nlu/streamlit/01_dashboard.py
+++
@@ -1,3 +0,0 @@
-import nlu
-nlu.enable_streamlit_caching() # Optional caching the models, recommended
-nlu.load('ner').viz_streamlit(['I love NLU !','I love Streamlit!'])
--- nlu/streamlit/05_classifiers.py
+++
@@ -1,3 +0,0 @@
-import nlu
-nlu.enable_streamlit_caching() # Optional caching the models, recommended
-nlu.load('sentiment').viz_streamlit_classes('I love NLU and Streamlit!')
